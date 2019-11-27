e7fd37e34c55a88a500a467ad6c6afefb236bc5c
Bill Hammond <willmhammond@gmail.com>
2019-11-26 19:59:57 -0800

    moved React and bootstrap packages from server to client

---
+++ client/src/App/assets/quotes.js
@@ -0,0 +1,99 @@
+const Quotes = [{
+        author: "F. Scott Fitzgerald",
+        quote: "First you take a drink, then the drink takes a drink, then the drink takes you."
+    },
+    {
+        author: "Lu Yu",
+        quote: "Its liquor is like the sweetest dew from Heaven."
+    },
+    {
+        author: "Ogden Nash",
+        quote: "Candy is dandy, but liquor is quicker."
+    },
+    {
+        author: "Ernest Hemingway",
+        quote: "Always do sober what you said you'd do drunk. That will teach you to keep your mouth shut."
+    },
+    {
+        author: "William Butler Yeats",
+        qoute: "The worst thing about some men is that when they are not drunk they are sober."
+    },
+    {
+        author: "Richard Brinsley Sheridan",
+        qoute: "A bumper of good liquor will end a contest quicker than justice, judge, or vicar."
+    },
+    {
+        author: "Seneca",
+        qoute: "Drunkenness is nothing but voluntary madness."
+    },
+    {
+        author: "Jonathon Swift",
+        qoute: "Better belly burst than good liquor be lost."
+    },
+    {
+        author: "Mary Wilson Little",
+        qoute: "Almost anything can be preserved in alcohol, except health, happiness, and money."
+    },
+    {
+        author: "Oscar Wilde",
+        qoute: "Alcohol, taken in sufficient quantities, may produce all the effects of drunkenness.  "
+    },
+    {
+        author: "Frank Sinatra",
+        qoute: "Alcohol may be man's worst enemy, but the bible says love your enemy."
+    },
+    {
+        author: "Steve Fergosi",
+        qoute: "A drunk man's words are a sober man's thoughts."
+    },
+    {
+        author: "Benjamin Franklin",
+        qoute: "In wine there is wisdom, in beer there is Freedom, in water there is bacteria."
+    },
+    {
+        author: "Philip Rosenthal",
+        qoute: "Drinking makes you happy with your body the way it is."
+    },
+    {
+        author: "F. Scott Fitzgerald",
+        qoute: "It's a great advantage not to drink among hard-drinking people. You can hold your tongue and, moreover, you can time any little irregularity of your own so that everybody else is so blind that they don't see or care."
+    },
+    {
+        author: "Finley Peter Dunne",
+        qoute: "Alcohol is necessary for a man so that he can have a good opinion of himself, undisturbed by the facts."
+    },
+    {
+        author: "W. C. Fields",
+        qoute: "There are only two real ways to get ahead today - sell liquor or drink it"
+    },
+    {
+        author: "George Bernard Shaw",
+        qoute: "In wine there is wisdom, in beer there is Freedom, in water there is bacteria."
+    },
+    {
+        author: "Dorothy Parker",
+        qoute: "I like to have two martini's,two at the very most. After three I'm under the table, after four I'm under the host."
+    },
+    {
+        author: "G.K Chesterton",
+        qoute: "Drink because you are happy, but never becauseyou are miserable."
+    },
+    {
+        author: "Oscar Wilde",
+        qoute: "After the first glass, you see things as you wish they were. After the second, you see things as they are not. Finally,you see things as they really are, and that is the most horrible thing in the world.."
+    },
+    {
+        author: "Nietzshe",
+        qoute: "For art to exist, for any sort of aesthetic activity or perception to exist, a certain physiological precondition is indispensable: intoxication."
+    },
+    {
+        author: "Tom  Waits",
+        qoute: "I don't have a drinking problem 'Cept when I can't get a drink."
+    },
+    {
+        author: "Tom  Waits",
+        qoute: "I'd rather have a bottle in front of me than a frontal lobotomy."
+    }
+]
+
+export default Quotes;
