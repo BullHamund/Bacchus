16d18ae6fe1be651ef8fd2ff87c276e7b5b9598f
Bill Hammond <willmhammond@gmail.com>
2019-12-02 06:26:07 -0800

    connected api's to to server

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const path = require('path');
+//const { join } = require("path");
 //const cookieParser = require('cookie-parser');
 const app = express();
+const routes = require("./routes/api");
+
+//app.use(morgan("dev"));
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
+    .use;
+
+app.use(routes);
 
 // An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
+// app.get('/api/getList', (req,res) => {
+//     var list = ["item1", "item2", "item3"];
+//     res.json(list);
+//     console.log('Sent list of items');
+// });
 
-// Handles any requests that don't match the ones above
+// // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile('/client/build/index.html');
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
