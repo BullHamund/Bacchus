2695f7d45715cd47bb0fbb46b3c0ce14d0f1e0db
Bill Hammond <willmhammond@gmail.com>
2019-12-02 06:31:35 -0800

    updated serever.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use(routes);
 
+
 // An api endpoint that returns a short list of items
 // app.get('/api/getList', (req,res) => {
 //     var list = ["item1", "item2", "item3"];
