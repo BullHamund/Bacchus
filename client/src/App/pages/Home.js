a9b9627d13c838be470f310612e57f921216f889
Bill Hammond <willmhammond@gmail.com>
2019-12-04 06:05:28 -0800

    fixed client/src/pages/Home.js; servers back up again

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import {
-  Card, Button, CardImg, CardTitle, Container, Row, Col ,CardText, CardColumns,
-  CardSubtitle, CardBody, Jumbotron, 
-  InputGroup, InputGroupAddon, InputGroupText, Input
-} from 'reactstrap';
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  Container,
+  Row,
+  Col,
+  CardText,
+  CardColumns,
+  CardSubtitle,
+  CardBody,
+  Jumbotron,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input
+} from "reactstrap";
 import { RecipeList, RecipeListItem } from "../components/RecipeList";
-import styled from 'styled-components';
+import styled from "styled-components";
 import API from "../utils/API";
- 
+
 const Styles = styled.div``;
-
-
 
 class Home extends Component {
   state = {
@@ -36,68 +47,79 @@ class Home extends Component {
       .catch(err => console.log(err));
   };
   render() {
-  return(<div>
-<Styles>
- <Jumbotron fluid className="Jumbo">
-    <Container fluid>
-      <h1>Cheers to the Good Times</h1>
+    return (
+      <div>
+        <Styles>
+          <Jumbotron fluid className="Jumbo">
+            <Container fluid>
+              <h1>Bacchus must now his power resign...</h1>
 
-        <InputGroup>
-        <Input
-                        name="recipeSearch"
-                        value={this.state.recipeSearch}
-                        onChange={this.handleInputChange}
-                        placeholder="Search For Drinks"
-                      />
-        <InputGroupAddon addonType="append">
-        <Button
-                        onClick={this.handleFormSubmit}
-                        type="success"
-                        className="input-lg"
-                      >
-                        Search
-                      </Button>
-          {/* <InputGroupText>Search</InputGroupText> */}
-        </InputGroupAddon>
-      </InputGroup>
-    </Container>
-  </Jumbotron>
-</Styles>
-​
-<div>
-  
-    <Container>
-    <Row>
-            <Col size="xs-12">
-              {!this.state.recipes.length ? (
-
-                <h1 className="text-center"></h1>
-
-                <h1 className="text-center">No Drinks to Display</h1>
-
-              ) : (
-                <RecipeList>
-                  {this.state.recipes.map(recipe => {
-                    return (
-                      <RecipeListItem
-                        key={recipe.strDrink}
-                        title={recipe.strDrink}
-                        
-                        ingredients={recipe.strInstructions}
-                        thumbnail={recipe.strDrinkThumb}
-                      />
-                    );
-                  })}
-                </RecipeList>
-              )}
-            </Col>
-          </Row>
- </Container>
-  
-  
-</div>
-​
-  </div>);
+              <InputGroup>
+                <Input
+                  name="recipeSearch"
+                  value={this.state.recipeSearch}
+                  onChange={this.handleInputChange}
+                  placeholder="Search For Drinks"
+                />
+                <InputGroupAddon addonType="append">
+                  <Button
+                    onClick={this.handleFormSubmit}
+                    type="success"
+                    className="input-lg"
+                  >
+                    Search
+                  </Button>
+                  {/* <InputGroupText>Search</InputGroupText> */}
+                </InputGroupAddon>
+              </InputGroup>
+            </Container>
+          </Jumbotron>
+        </Styles>
+        ​
+        <div>
+          <Container>
+            <Row>
+              <Col size="xs-12">
+                {!this.state.recipes.length ? (
+                  <h1 className="text-center">No Drinks to Display</h1>
+                ) : (
+                  <RecipeList>
+                    {this.state.recipes.map(recipe => {
+                      return (
+                        <RecipeListItem
+                          key={recipe.strDrink}
+                          title={recipe.strDrink}
+                          ingredients={recipe.strInstructions}
+                          thumbnail={recipe.strDrinkThumb}
+                        />
+                      );
+                    })}
+                  </RecipeList>
+                )}
+              </Col>
+            </Row>
+          </Container>
+          <Container>
+            <Row>
+              <Col>Our Story</Col>
+            </Row>
+            <br></br>
+            tas ir teksta salikums, kuru izmanto poligrāfijā un maketēšanas
+            darbos. Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju
+            kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs
+            izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu
+            paraugiem. Tas ir ne tikai pārdzīvojis piecus gadsimtus, bet bez
+            ievērojamām izmaiņām saglabājies arī mūsdienās, pārejot uz
+            datorizētu teksta apstrādi. Tā popularizēšanai 60-tajos gados
+            kalpoja Letraset burtu paraugu publicēšana ar Lorem Ipsum teksta
+            fragmentiem un, nesenā pagātnē, tādas maketēšanas programmas kā
+            Aldus PageMaker, kuras šablonu paraugos ir izmantots Lorem Ipsum
+            teksts.
+          </Container>
+        </div>
+        ​
+      </div>
+    );
   }
 }
 export default Home;
