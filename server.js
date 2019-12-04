bf09f69b50121ec4d82021f2f6a1e4bc0ea548f2
Bill Hammond <willmhammond@gmail.com>
2019-12-04 05:05:47 -0800

    updated server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path');
 const app = express();
 const routes = require("./routes");
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-app.use(express.static("public"));
-
-
-=======
 app.use(express.static("public"));
 
 app.use(routes);
