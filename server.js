e4386554afd937c5d35fd2fe72a48ea0c4fa73b6
isai villanueva~git configssh-keygen -t rsa -b 4096 -C isai.villanu.7@gmail.comgit config --global user.name isai <isai.villanu.7@gmail.com>
2019-12-03 20:20:57 -0800

    deleting

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const path = require('path');
-//const cookieParser = require('cookie-parser');
 const app = express();
 const routes = require("./routes");
-
-
-// Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 
 app.use(express.static("public"));
 
-// Handles any requests that don't match the ones above
-// app.get('*', (req,res) =>{
-//    // res.sendFile('./client/build/index.html');
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
 
-// });
 app.use(routes);
 
 const port = process.env.PORT || 3001;
