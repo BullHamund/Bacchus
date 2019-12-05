6fd42a7a05d5623c688f6208ee18d2be13ca7428
isai villanueva~git configssh-keygen -t rsa -b 4096 -C isai.villanu.7@gmail.comgit config --global user.name isai <isai.villanu.7@gmail.com>
2019-12-04 16:27:31 -0800

    messing with nav

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Page from './components/Page';
-import Footer from './components/Footer';
 import Navigation from './components/Navbar';
 import Loading from "./components/Loading";
 import Profile from "./views/Profile";
 import { useAuth0 } from "./react-auth0-spa";
 import history from "./utils/history";
 import PrivateRoute from "./components/PrivateRoute";
-import {Container} from 'reactstrap';
 import Articles from './pages/Articles.js';
 import Events from './pages/Events.js';
-import Search from './pages/SearchResults.js';
+import {Container} from 'react-bootstrap';
 
 
 const App = () => {
@@ -24,17 +22,17 @@ const App = () => {
       return (
         <Router history={history}>
           <div id="app" className="d-flex flex-column h-100">
-          <Navigation/>
+          
+          <Navigation color="transparent"/>
         
-            <Container className="flex-grow-1 mt-5">
               <Switch>
                 <Route path="/" exact component={Page} />
                 <Route path="/articles" exact component={Articles} />
                 <Route path="/events" exact component={Events} />
-                <Route path="/search" exact component={Search} />
+                {/* <Route path="/search" exact component={Search} /> */}
                 <PrivateRoute path="/profile" component={Profile} />
               </Switch>
-            </Container>
+             
           </div>
         </Router>
       );
