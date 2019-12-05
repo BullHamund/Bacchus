b6307ab63f4cf0fb9ad4121763a1eba91e566a07
Luigi Cruz <angeloluigicruz@gmail.com>
2019-12-04 20:05:19 -0800

    ADDED CARDS TO EVENTS AND ARTICLES

--- client/src/App/pages/Events.js
+++ client/src/App/pages/Events.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
 
 function Events() {
-return(<div>Events</div>);
+return(<div>
+    <h1>Events</h1>
+    <Row>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Event 1</CardTitle>
+          <CardText>Event Information</CardText>
+          <Button>Learn More</Button>
+        </Card>
+      </Col>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Event 2</CardTitle>
+          <CardText>Event Information</CardText>
+          <Button>Learn More</Button>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Event 3</CardTitle>
+          <CardText>Event Information</CardText>
+          <Button>Learn More</Button>
+        </Card>
+      </Col>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Event 4</CardTitle>
+          <CardText>Event Information</CardText>
+          <Button>Learn More</Button>
+        </Card>
+      </Col>
+    </Row>
+</div>);
 }
 
 export default Events;
